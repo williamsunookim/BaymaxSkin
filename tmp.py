7291594dd033e200017ec00bec233b72e2f8169e
williamsunookim <koreasunoo@dgist.ac.kr>
2026-04-05 17:42:56 +0900

    onnx file optimized

--- tmp.py
+++ tmp.py
@@ -1,9 +1,9 @@
-import torch
-from verify_series_model import jointPointcloud
+import onnx
+from onnxruntime.quantization import QuantType
+from onnxruntime.quantization import quantize_dynamic
 
-model = jointPointcloud()
-model.load_state_dict(torch.load('series_model_best.pt', map_location=torch.device('cpu')))
-model.eval()
-dummy = torch.zeros(1, 16)
-torch.onnx.export(model, dummy, 'static/models/series_model_best.onnx',
-                  input_names=['input'], output_names=['output'])
+model_fp32 = 'static/models/series_model_best.onnx'
+model_quant = 'static/models/series_model_best_quant.onnx'
+
+# 동적 양자화 수행
+quantize_dynamic(model_fp32, model_quant, weight_type=QuantType.QUInt8)
