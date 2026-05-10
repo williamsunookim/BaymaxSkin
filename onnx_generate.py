e1840a6c141c45e5495c683106cf5daf10555660
williamsunookim <koreasunoo@dgist.ac.kr>
2026-05-10 22:26:54 +0900

    Limited ROM

--- onnx_generate.py
+++ onnx_generate.py
@@ -5,5 +5,4 @@ from onnxruntime.quantization import quantize_dynamic
 model_fp32 = 'static/models/series_model_best.onnx'
 model_quant = 'static/models/series_model_best_quant.onnx'
 
-# 동적 양자화 수행
 quantize_dynamic(model_fp32, model_quant, weight_type=QuantType.QUInt8)
